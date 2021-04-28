c829d5543d8be16bcbb14720d46d5f4342b1a4dd
juan garcia <juan.dw.ft@gmail.com>
2021-04-28 18:20:10 -0500

    Add service object to get vehicle attribute from fleet.io

---
+++ app/services/vehicle_attributes.rb
@@ -0,0 +1,23 @@
+class VehicleAttributes
+  require 'uri'
+  require 'net/http'
+  require 'httparty'
+
+  def initialize vin
+    @vin = vin
+  end
+
+  def get
+    url = "https://secure.fleetio.com/api/v1/vehicles?q[vin_eq]=#{@vin}"
+
+    headers = {
+      'Authorization': "Token 635d9bf526682e02ab8abcf05b00561d50de8142",
+      'Account-Token': "798819214b"
+    }
+
+    response = HTTParty.get(url, headers: headers)
+    vehicle = JSON.parse(response.body).first
+
+    { vin: vehicle['vin'], color: vehicle['vehicle_status_color'], status: vehicle['vehicle_status_name'], vehicle_type: vehicle['vehicle_type_name'], fleet_id: vehicle['id'] }
+  end
+end
